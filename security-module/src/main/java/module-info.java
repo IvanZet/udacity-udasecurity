c978d810c0f3bf10a691573e9ce044d0efae3082
Ivan Zykov <ivan.v.zykov@gmail.com>
2022-06-24 16:32:55 +0200

    Splits project into a parent and two child modules

---
+++ security-module/src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.udacity.security {
+    requires com.udacity.image;
+    requires miglayout.swing;
+    requires java.desktop;
+    requires com.google.common;
+    requires gson;
+    requires java.prefs;
+}
---
+++ security-module/src/test/java/com/udacity/security/AppTest.java
@@ -0,0 +1,20 @@
+package com.udacity.security;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
